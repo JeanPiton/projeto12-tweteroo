bfec0c50146ae9ae6668afa6b85a189b36ff453d
JeanPiton <jean.orpiton@gmail.com>
2023-06-22 20:52:48 -0300

    Post /tweets

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 
 let user = {username:"", avatar:""}
-const tweets = [{username:"",avatar:"",tweet:""}]
+const tweets = []
 
 const app = express()
 app.use(express.json())
@@ -16,6 +16,23 @@ app.post("/sign-up",(req,res)=>{
     }
     user = login
     res.sendStatus(200)
+})
+
+app.post("/tweets",(req,res)=>{
+    if(!req.body.username){
+        res.send("UNAUTHORIZED")
+    }else{
+        const message = {
+            username: req.body.username,
+            tweet: req.body.tweet,
+            avatar: user.avatar
+        }
+        tweets.push(message)
+        if(tweets.length>10){
+            tweets.shift()
+        }
+        res.sendStatus(200)
+    }
 })
 
 app.get("/tweets",(req,res)=>{
