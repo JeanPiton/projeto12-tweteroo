b35f831713f9df2327c18adc862f54d2ee39e924
JeanPiton <jean.orpiton@gmail.com>
2023-06-22 20:35:30 -0300

    Post /sign-up

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import express from "express"
 import cors from "cors"
 
+let user = {username:"", avatar:""}
+const tweets = [{username:"",avatar:"",tweet:""}]
+
 const app = express()
+app.use(express.json())
 app.use(cors())
 app.listen(5000,()=>console.log("Server running on port 5000"))
+
+app.post("/sign-up",(req,res)=>{
+    let login = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
+    user = login
+    res.sendStatus(200)
+})
+
+app.get("/tweets",(req,res)=>{
+    res.send(tweets)
+})
